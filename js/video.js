e60aab4c4b30b066a0f81a3b5721846c73188b66
Zach Levy <zacharyalevy@gmail.com>
2014-02-22 15:56:59 -0500

    [client] basic effect and refactor

--- js/video.js
+++ js/video.js
@@ -1,24 +1,57 @@
+// set speed
+var speed = 1.0;
+// get data how often
 
-var speed = 3.0;
-
+// video variable
 video = document.getElementById("bgvid");
+// play it broooo when page loaded
 video.play();
 
+$( "#img-score" ).hide();
+
+// set the speed of the video while its playing
 function setSpeed () {
     var x=document.getElementById("demo");
     speed = Math.floor((Math.random()*3)+1);
 }
 
-function everySecond() {
-    window.setTimeout(function() {
-        video.playbackRate=speed;
-        $( "#score" ).toggle( "explode" );
-
-    }, 1000);
-    $( "#score" ).toggle( "show" );
+function getBikeData () {
+	var bikeData;
+	// hit the server brooo
+	$.getJSON("http://127.0.0.1:8080/horizon-update", function(json){
+		bikeData = json;
+		console.log(bikeData);
+		processData(bikeData);
+	});
 }
 
-everySecond();
+function processData (bikeData) {
+	var distance = 0;
+	distance = bikeData.distance;
+}
+
+function test () {
+	console.log('test');
+	$( "#img-score" ).toggle( "explode", function() {
+		window.setInterval(function() {
+			$( "#img-score" ).toggle( "explode");
+		},1000);
+    	;
+  });
+}
+
+// update every x milliseconds
+function everyTime(often) {
+    window.setTimeout(function() {
+
+ 		// change the video speed
+        video.playbackRate = speed;
+        // every second, run test function
+        test();
+    }, often);
+}
+
+everyTime(1000);
 
 
 
