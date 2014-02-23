89faa003312956660a48465a0fbef6d01f77c5d0
Zach Levy <zacharyalevy@gmail.com>
2014-02-23 13:26:57 -0500

    [client] demo version

--- js/video.js
+++ js/video.js
@@ -2,27 +2,53 @@
 var finish = 1000;
 var maxVelo = 40;
 var distOffset = 0;
+var totalPoints = 0;
+var targetVelo = 10;
+
+var videos = new Array("nyc_bike.mp4", "canyon_bike.mp4", "mountain_bike.mp4");
 
 var goal1 = {
 	atDist:100,
 	points:true,
+	addPoints: 100,
 	baseText:"+100 Points",
-	secondaryText:"+121 Bonus"
+	secondaryText:"+121 Bonus",
+	used: false
 };
 var goal2 = {
 	atDist:200,
 	points:true,
-	baseText:"+100 Points",
-	secondaryText:"+122 Bonus"
+	addPoints: 200,
+	baseText:"+200 Points",
+	secondaryText:"+122 Bonus",
+	used: false
 };
 var goal3 = {
-	atDist:300,
+	atDist:500,
 	points:true,
-	baseText:"+100 Points",
-	secondaryText:"+123 Bonus"
+	addPoints: 500,
+	baseText:"+500 Points",
+	secondaryText:"+123 Bonus",
+	used: false
+};
+var goal4 = {
+	atDist:750,
+	points:true,
+	addPoints: 750,
+	baseText:"+750 Points",
+	secondaryText:"+123 Bonus",
+	used: false
+};
+var goal5 = {
+	atDist:1000,
+	points:true,
+	addPoints: 1000,
+	baseText:"+1000 Points",
+	secondaryText:"+123 Bonus",
+	used: false
 };
 
-var goals = new Array(goal1,goal2,goal3);
+var goals = new Array(goal1,goal2,goal3, goal4, goal5);
 
 console.log(goals);
 
@@ -37,10 +63,19 @@ video.play();
 
 $( "#img-score" ).hide();
 
+/*
 // set the speed of the video while its playing
 function setSpeed () {
     var x=document.getElementById("demo");
     speed = Math.floor(10 * (Math.random()*3)+1)/10);
+}
+*/
+
+// calc the bonus score
+function calcBonus (basePoints) {
+	var bonus = Math.round((Math.pow(2,(1.0/(Math.random()+0.2)))/3) * basePoints);
+	totalPoints += bonus;
+	return bonus;
 }
 
 function getBikeData () {
@@ -67,11 +102,38 @@ function processData (bikeData) {
 	velo = bikeData.velocity;
 	writeData(dist, revs, freq, velo);
 	updateSpeed(velo);
+	checkGoals(dist);
+	checkVelo(velo);
+	updateScore();
+	checkFinish(dist);
+}
+
+function checkVelo (velo) {
+	if (velo > targetVelo) {
+		totalPoints = Math.round(totalPoints + (targetVelo / 3));
+	}
+}
+
+function updateScore(){
+	document.getElementById("total-points").innerHTML = totalPoints;
+}
+
+function checkGoals(dist) {
+	for (var i = 0 ;i < goals.length; i++) {
+		if (dist > goals[i].atDist && goals[i].used == false) {
+			
+			textExplode();
+			totalPoints += goals[i].addPoints;
+			var bonus = calcBonus(goals[i].addPoints);
+			updateText(goals[i].baseText, "+" + bonus + " Bonus");
+			goals[i].used = true;
+		}
+	};
 }
 
 function updateSpeed(velocity) {
 	if (velocity > 1) {
-		var speed = (velocity * 2.5) / 10.0;
+		var speed = (velocity * 2) / 10.0;
 	} else {
 		var speed = 0;
 	}
@@ -91,9 +153,25 @@ function writeData (dist, revs, freq, velo) {
 	document.getElementById("progress-wrap").innerHTML = "<progress id=\"distance-bar\" value=\"" + progress + "\" max=\"100\"></progress>";
 }
 
+// html for text explode
+function updateText (primary, secondary) {
+	document.getElementById("base-text").innerHTML = primary;
+	document.getElementById("secondary-text").innerHTML = secondary;
+}
+
+function showText () {
+	$("#base-text").show();
+	$("#secondary-text").show();
+}
+
+function hideText() {
+	$("#base-text").hide();
+	$("#secondary-text").hide();
+}
+
+// animation for text
 function textExplode () {
 	$( "#text-wrap" ).hide();
-	console.log('test');
 	$( "#text-wrap" ).toggle( "explode", function() {
 		setTimeout(function () {
 			$( "#text-wrap" ).toggle( "explode");
@@ -106,17 +184,89 @@ function textExplode () {
 function everyTime() {
     getBikeData();
 	// change the video speed
-    
-    // every second, run test function
-    
+}
+
+
+
+function timer() {
+	
+}
+
+function waitingState () {
+	showText();
+	timeout = 3;
+	var timeoutinProgress = true;
+	console.log('waiting state');
+	var counter = setInterval(function(){
+	//setInterval(function(){
+		
+		updateText("Time Until Next Heat", timeout + " seconds");
+		console.log(timeout);
+
+		if (timeout <= 0 && timeoutinProgress == true){ 
+			// done	
+			hideText();
+			resetDistance();
+			resetGoals();
+			timeoutinProgress = false;	
+		  	clearInterval(counter);
+		  	resetVideo();
+		}
+	timeout -= 1;
+
+
+	}, 1000);
+}
+
+function resetVideo () {
+	//selects a video from the array
+	console.log('reset video function');
+	var newVideo = videos[Math.floor(Math.random()*videos.length)];
+	console.log(newVideo);
+	document.getElementById("vid-wrap").innerHTML = "<video id=\"bgvid\"><source src=\"media/" + newVideo + "\" type=\"video/mp4\"></video>";
+	video = document.getElementById("bgvid");
+	video.play();
+}
+
+//
+function checkFinish(dist) {
+	if (dist > finish) {
+		resetDistance();
+		resetTrack();
+		//setTimeout(resetTrack(), 1000);
+	}
+}
+
+// sets game for next heat
+function resetTrack () {
+	waitingState();
+	//resetDistance();
+	//resetGoals ();
+	clearUI();
+	
+}
+
+function resetGoals () {
+	for (var i = 0 ;i < goals.length; i++) {
+		goals[i].used = false;
+	}
+}
+
+function clearUI () {
+
 }
 
 setInterval(everyTime, 500);
 
-textExplode();
+// sets distance since browser reload
 
-$.getJSON("http://localhost:8080/", function(json){
+
+
+function resetDistance () {
+	$.getJSON("http://localhost:8080/", function(json){
 	bikeData = json;
 	distOffset = bikeData.distance;
 });
+}
 
+resetDistance();
