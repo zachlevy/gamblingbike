f8e08eec2b9f5bc7f613343a88237f7493e4608e
Zach Levy <zacharyalevy@gmail.com>
2014-02-22 23:51:17 -0500

    adding smooth progress bar; distance offset

--- js/video.js
+++ js/video.js
@@ -1,6 +1,7 @@
 // track
 var finish = 1000;
 var maxVelo = 40;
+var distOffset = 0;
 
 var goal1 = {
 	atDist:30,
@@ -25,10 +26,6 @@ var goals = new Array(goal1,goal2,goal3);
 
 console.log(goals);
 
-
-
-
-
 // set speed
 var speed = 1.0;
 // get data how often
@@ -49,7 +46,7 @@ function setSpeed () {
 function getBikeData () {
 	var bikeData;
 	// hit the server brooo
-	$.getJSON("http://localhost:8082/", function(json){
+	$.getJSON("http://localhost:8080/", function(json){
 		bikeData = json;
 		//console.log(bikeData);
 		processData(bikeData);
@@ -58,15 +55,18 @@ function getBikeData () {
 
 // what to do with all that data? process it.
 function processData (bikeData) {
+	// $(document).ready() {
+	// 	var distoffset = bikeData.distance;
+	// }
 	revs = bikeData.revolutions;
 	freq = bikeData.frequency;
 	// meters
-	dist = bikeData.distance;
+	
+	dist = (bikeData.distance - distOffset);
 	// meters per second
 	velo = bikeData.velocity;
 	writeData(dist, revs, freq, velo);
 	updateSpeed(velo);
-	
 }
 
 function updateSpeed(velocity) {
@@ -84,8 +84,8 @@ function writeData (dist, revs, freq, velo) {
 	document.getElementById("total-dist").innerHTML = Math.round(dist) + " m";
 	document.getElementById("velocity").innerHTML = Math.round(velo * 3.6 ) + " km/h";
 	//console.log(dist);
-	if (dist < finish && dist > 0) {
-		var progress = Math.round((dist / finish) * 100);
+	if (dist < finish ) {
+		var progress = Math.round((dist / finish) * 1000)/10;
 	} else {
 		var progress = 100;
 	}
@@ -116,6 +116,8 @@ setInterval(everyTime, 500);
 
 textExplode();
 
-
-
+$.getJSON("http://localhost:8080/", function(json){
+	bikeData = json;
+	distOffset = bikeData.distance;
+});
 
