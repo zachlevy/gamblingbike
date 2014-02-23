d7ba4ce2448148af96f5a5a2041dfe69e4ab482d
Zach Levy <zacharyalevy@gmail.com>
2014-02-22 19:55:36 -0500

    [client] failsafe. basic HUD

--- js/video.js
+++ js/video.js
@@ -1,3 +1,34 @@
+// track
+var finish = 1000;
+var maxVelo = 40;
+
+var goal1 = {
+	atDist:30,
+	points:true,
+	baseText:"+100 Points",
+	secondaryText:"+124 Bonus"
+};
+var goal2 = {
+	atDist:130,
+	points:true,
+	baseText:"+100 Points",
+	secondaryText:"+124 Bonus"
+};
+var goal3 = {
+	atDist:30,
+	points:true,
+	baseText:"+100 Points",
+	secondaryText:"+124 Bonus"
+};
+
+var goals = new Array(goal1,goal2,goal3);
+
+console.log(goals);
+
+
+
+
+
 // set speed
 var speed = 1.0;
 // get data how often
@@ -18,46 +49,66 @@ function setSpeed () {
 function getBikeData () {
 	var bikeData;
 	// hit the server brooo
-	$.getJSON("http://192.168.1.101:8082/", function(json){
+	$.getJSON("http://169.254.9.110:8080/", function(json){
 		bikeData = json;
-		console.log(bikeData);
+		//console.log(bikeData);
 		processData(bikeData);
 	});
 }
 
+// what to do with all that data? process it.
 function processData (bikeData) {
-	var distance = 0;
-	dist = bikeData.distance;
 	revs = bikeData.revolutions;
 	freq = bikeData.frequency;
+	// meters
+	dist = bikeData.distance;
+	// meters per second
 	velo = bikeData.velocity;
+	writeData(dist, revs, freq, velo);
+	updateSpeed(velo);
+	
 }
 
-function test () {
+function updateSpeed(velocity) {
+	if (velocity > 1) {
+		var speed = (velocity * 2.5) / 10;
+	} else {
+		var speed = 0;
+	}
+	
+	video.playbackRate = speed;
+} 
+
+// put it on the page brooo
+function writeData (dist, revs, freq, velo) {
+	document.getElementById("total-dist").innerHTML = Math.round(dist) + " m";
+	document.getElementById("velocity").innerHTML = Math.round(velo * 3.6 ) + " km/h";
+	//console.log(dist);
+}
+
+function textExplode () {
+	$( "#text-wrap" ).hide();
 	console.log('test');
-	$( "#img-score" ).toggle( "explode", function() {
-		window.setInterval(function() {
-			$( "#img-score" ).toggle( "explode");
-		},1000);
-    	;
+	$( "#text-wrap" ).toggle( "explode", function() {
+		setTimeout(function () {
+			$( "#text-wrap" ).toggle( "explode");
+		}, 3000);
+		
   });
 }
 
 // update every x milliseconds
-function everyTime(often) {
-    window.setTimeout(function() {
-    	getBikeData();
-
- 		// change the video speed
-        video.playbackRate = speed;
-        // every second, run test function
-        test();
-    }, often);
+function everyTime() {
+    getBikeData();
+	// change the video speed
+    
+    // every second, run test function
+    
 }
 
-everyTime(1000);
+setInterval(everyTime, 500);
 
-
+textExplode();
 
 
 
